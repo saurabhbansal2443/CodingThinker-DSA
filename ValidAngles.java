66e578acdf8f85974148ae85060ab83a5bd90230
saurabhbansal2443 <saurabhbansal2443@gmail.com>
2025-09-18 20:35:32 +0530

    test even odd

--- ValidAngles.java
+++ ValidAngles.java
@@ -10,6 +10,7 @@ public class ValidAngles {
         int angle1 = sc.nextInt();
         int angle2 = sc.nextInt();
         int angle3 = sc.nextInt();
+        
         int sum = angle1 + angle2 + angle3;
         if (sum == 180) {
             System.out.println("This is a valid triangle");
