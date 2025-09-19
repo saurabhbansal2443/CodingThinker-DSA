66eecb27a118453234157611cfb88e3191eafcf1
saurabhbansal2443 <saurabhbansal2443@gmail.com>
2025-09-19 20:33:28 +0530

    conditional operator

--- checkConsonantOrVowel.java
+++ checkConsonantOrVowel.java
@@ -10,15 +10,32 @@ public class checkConsonantOrVowel {
         char ch = sc.next().charAt(0);
         // vowel -> a,e,i ,o ,u
 
-        if (ch == 'a') {
-            System.out.println("It is a vowel");
-        } else if (ch == 'e') {
-            System.out.println("It is a vowel");
-        } else if (ch == 'i') {
-            System.out.println("It is a vowel");
-        } else if (ch == 'o') {
-            System.out.println("It is a vowel");
-        } else if (ch == 'u') {
+        // if (ch == 'a') {
+        // System.out.println("It is a vowel");
+        // } else if (ch == 'e') {
+        // System.out.println("It is a vowel");
+        // } else if (ch == 'i') {
+        // System.out.println("It is a vowel");
+        // } else if (ch == 'o') {
+        // System.out.println("It is a vowel");
+        // } else if (ch == 'u') {
+        // System.out.println("It is a vowel");
+        // } else if (ch == 'A') {
+        // System.out.println("It is a vowel");
+        // } else if (ch == 'E') {
+        // System.out.println("It is a vowel");
+        // } else if (ch == 'I') {
+        // System.out.println("It is a vowel");
+        // } else if (ch == 'O') {
+        // System.out.println("It is a vowel");
+        // } else if (ch == 'U') {
+        // System.out.println("It is a vowel");
+        // } else {
+        // System.out.println("It is a consonant");
+        // }
+
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I'
+                || ch == 'O' || ch == 'U') {
             System.out.println("It is a vowel");
         } else {
             System.out.println("It is a consonant");
