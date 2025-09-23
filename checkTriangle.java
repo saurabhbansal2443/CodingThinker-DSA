2009fc062266881cf51b04c173f4634f85543ab3
saurabhbansal2443 <saurabhbansal2443@gmail.com>
2025-09-23 21:03:02 +0530

    HomeWork

--- checkTriangle.java
+++ checkTriangle.java
@@ -13,9 +13,16 @@ public class checkTriangle {
 
         Scanner sc = new Scanner(System.in);
 
+        // Homerwork to do this question without using && and ||  operator
+
+        // a 10 10 12 10 12 20 9 20
+        // b 9 10 12 12 10 25 6 22
+        // c 8 10 10 12 12 10 5 20
+
         int a = sc.nextInt();
         int b = sc.nextInt();
         int c = sc.nextInt();
+
         if (a == b && b == c) {
             System.out.println("The traingle is equilateral ");
         } else if (a == b || b == c || c == a) {
@@ -23,6 +30,6 @@ public class checkTriangle {
         } else {
             System.out.println("The traingle is scalene ");
         }
-        
+
     }
 }
