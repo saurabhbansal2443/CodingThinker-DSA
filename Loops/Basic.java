dda0688d8ca22680aa7ca3abe402f9731b659532
saurabhbansal2443 <saurabhbansal2443@gmail.com>
2025-10-07 21:59:06 +0530

    Loops started

---
+++ Loops/Basic.java
@@ -0,0 +1,25 @@
+package Loops;
+
+public class Basic {
+    public static void main(String[] args) {
+
+        // Print 1 to 5
+        // for (int i = 1; i <= 5; i++) {
+        // System.out.println(i);
+        // }
+
+        // print All even number between 1 - 10
+
+        // for (int i = 1; i <= 5; i++) {
+
+        // if (i % 2 == 0) {
+        // System.out.println(i);
+        // }
+        // }
+
+        for (int i = 2; i <= 10; i += 2) {
+            System.out.println(i);
+        }
+
+    }
+}
