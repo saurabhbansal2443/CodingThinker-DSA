b37d2252c5fc6590c3d52c6b624dc07efbba68d1
saurabhbansal2443 <saurabhbansal2443@gmail.com>
2025-11-17 21:50:52 +0530

    Pattern F

--- Patterns/Pattern17.java
+++ Patterns/Pattern17.java
@@ -6,7 +6,7 @@ public class Pattern17 {
         int n = 9;
         for (int i = 1; i <= n; i++) { // row
             for (int j = 1; j <= n; j++) { // columns
-                if (i == 1 || i == (n / 2 + 1) || i == n || (j == 1 && i < (n / 2 + 1)) || (j == n && i > n / 2 + 1)) {
+                if ((i == 1 && j >= n / 2 + 1) || (i == n / 2 + 1 && j >= n / 2 + 1) || (j == n / 2 + 1)) {
                     System.out.print(" * ");
                 } else {
                     System.out.print("   ");
