175fd7c5abc772b32bcf61ad4dd5e3cc644f5126
saurabhbansal2443 <saurabhbansal2443@gmail.com>
2025-11-17 21:44:37 +0530

    Pattern S

---
+++ Patterns/Pattern17.java
@@ -0,0 +1,18 @@
+package Patterns;
+
+public class Pattern17 {
+    public static void main(String[] args) {
+        // S
+        int n = 9;
+        for (int i = 1; i <= n; i++) { // row
+            for (int j = 1; j <= n; j++) { // columns
+                if (i == 1 || i == (n / 2 + 1) || i == n || (j == 1 && i < (n / 2 + 1)) || (j == n && i > n / 2 + 1)) {
+                    System.out.print(" * ");
+                } else {
+                    System.out.print("   ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
