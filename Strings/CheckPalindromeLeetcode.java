52266e31d728a7eedc3a91e1825ce20833dab3a3
saurabhbansal2443 <saurabhbansal2443@gmail.com>
2025-12-30 23:10:43 +0530

    Leetcode

---
+++ Strings/CheckPalindromeLeetcode.java
@@ -0,0 +1,61 @@
+package Strings;
+
+// LEETCODE 125 
+//https://leetcode.com/problems/valid-palindrome/?envType=problem-list-v2&envId=string
+public class CheckPalindromeLeetcode {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Solution {
+    public boolean isPalindrome(String str) {
+        str = str.trim();
+        if (str.length() == 0 || str.length() == 1) {
+            return true;
+        }
+
+        String formatted = formatString(str);
+        return checkPlaindrome(formatted);
+
+    }
+
+    public String formatString(String str) {
+
+        if (str.length() == 0 || str.length() == 1) {
+            return str;
+        }
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            if (ch >= 'A' && ch <= 'Z') {
+                sb.append((char) (ch - 'A' + 'a'));
+            } else if (ch >= 'a' && ch <= 'z') {
+                sb.append(ch);
+            } else if (ch >= '0' && ch <= '9') {
+                sb.append(ch);
+            }
+        }
+        return sb.toString();
+    }
+
+    public boolean checkPlaindrome(String str) {
+        int start = 0;
+        int end = str.length() - 1;
+
+        while (start < end) {
+            char chEnd = str.charAt(end);
+            char chSt = str.charAt(start);
+
+            if (chEnd != chSt) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+
+}
